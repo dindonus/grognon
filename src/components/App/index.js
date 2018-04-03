a1204cde9b8007d4fd0f56c09f9129445e8eb863
Nicolas Cordin <n.cordin@meetic-corp.com>
2018-04-03 14:12:50 +0200

    configure more animations

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,10 +13,12 @@ const CaverManAnimation = animate(CaverManSprite);
 const App = () => {
   return (
     <div className="App">
-      <CaverManAnimation frames={[1, 2, 3, 4, 5]} fps={7} />
+      <CaverManAnimation frames={[1, 2, 3, 4, 5]} fps={6} />
       <CaverManAnimation frames={[6, 7, 8, 9, 10, 11, 12, 13]} fps={11} />
       <CaverManAnimation frames={[14, 15, 16]} fps={10} />
       <CaverManAnimation frames={[17, 18, 19]} fps={10} />
+      <CaverManAnimation frames={[1, 1, 25, 26, 27, 28]} fps={8} />
+      <CaverManAnimation frames={[1, 1, 1, 1, 29, 30, 31, 32, 33, 34, 35, 35, 35, 35]} fps={14} />
     </div>
   );
 };
